b4296856b081c107c910b31398662aab90573fc1
Holly <hollyabel@hotmail.co.uk>
2026-05-07 23:13:10 -0400

    “fix”

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def sentiment_analyzer(text_to_analyze):
+def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     
     # Constructing the request payload
