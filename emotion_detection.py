26334e9df674db8c48a2e8f505d50db7d6416d93
Holly <hollyabel@hotmail.co.uk>
2026-05-08 00:27:20 -0400

    “format”

--- emotion_detection.py
+++ emotion_detection.py
@@ -19,27 +19,28 @@ def emotion_detector(text_to_analyze):
     # Sending a POST request to the API
     response = requests.post(url, json=myobj, headers=headers)
     
-if response.status_code == 200:
-    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)        
-
-    desired_object = {
-        'anger': emotion_scores.get('anger', 0),
-        'disgust': emotion_scores.get('disgust', 0),
-        'fear': emotion_scores.get('fear', 0),
-        'joy': emotion_scores.get('joy', 0),
-        'sadness': emotion_scores.get('sadness', 0),
-        'dominant_emotion': dominant_emotion
-    }
-elif response.status_code == 400:
-    desired_object = {
-        'anger': None,
-        'disgust': None,
-        'fear': None,
-        'joy': None,
-        'sadness': None,
-        'dominant_emotion': None
-    }
+    if response.status_code == 200:
+        formatted_response = response.json()
+        emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        
+        desired_object = {
+            'anger': emotion_scores.get('anger', 0),
+            'disgust': emotion_scores.get('disgust', 0),
+            'fear': emotion_scores.get('fear', 0),
+            'joy': emotion_scores.get('joy', 0),
+            'sadness': emotion_scores.get('sadness', 0),
+            'dominant_emotion': dominant_emotion
+        }
+    elif response.status_code == 400:
+        desired_object = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     else:
         # Handle other unexpected responses
         desired_object = {
