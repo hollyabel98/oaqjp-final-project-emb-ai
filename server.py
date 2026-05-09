31bb270fabc2401c798e227d1b95c256dafde3a7
Holly <hollyabel@hotmail.co.uk>
2026-05-08 23:45:34 -0400

    “fix”

--- server.py
+++ server.py
@@ -13,22 +13,22 @@ def detect_emotion():
 
     # Extract the dominant emotion and its score from the response
     dominant_emotion = response['dominant_emotion']
-    score = response['emotion_scores'].get(dominant_emotion, 0)
-
-    # Return a formatted string with the dominant emotion and its score
-    return (
-        "For the given statement, the system response is anger: {}, disgust: {}, fear: {}, joy: {} and sadness: {}. "
-        "The dominant emotion is {}."
-        .format(
-            scores.get('anger'),
-            scores.get('disgust'),
-            scores.get('fear'),
-            scores.get('joy'),
-            scores.get('sadness'),
-            response['dominant_emotion']
-        ), 
-        200
-    )
+   score = response['emotion_scores'].get(dominant_emotion, 0)
+   
+# Return a formatted string with the dominant emotion and its score
+return (
+    "For the given statement, the system response is anger: {}, disgust: {}, fear: {}, joy: {} and sadness: {}. "
+    "The dominant emotion is {}."
+    .format(
+        response['emotion_scores'].get('anger'),
+        response['emotion_scores'].get('disgust'),
+        response['emotion_scores'].get('fear'),
+        response['emotion_scores'].get('joy'),
+        response['emotion_scores'].get('sadness'),
+        response['dominant_emotion']
+    ), 
+    200
+)
 
 @app.route("/")
 def render_index_page():
